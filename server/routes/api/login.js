f49642b5a6ab53e66c7b28f75e9d31e938f2dcee
hexie <hx3329@gmail.com>
2018-09-02 12:18:42 +1000

    config and add user component in server

---
+++ server/routes/api/login.js
@@ -0,0 +1,85 @@
+const User = require('../../models/User');
+
+module.exports = (app) => {
+  /*
+   * Sign up
+   */
+  app.post('/api/account/signup', (req, res, next) => {
+    const { body } = req;
+    const {
+      firstName,
+      lastName,
+      password
+    } = body;
+
+    let {
+      email
+    } = body;
+
+    if (!firstName) {
+      return res.send({
+        success: false,
+        message: 'Error: firstName cannot be blank.'
+      });
+    }
+
+
+    if (!lastName) {
+      return res.send({
+        success: false,
+        message: 'Error: lastName cannot be blank.'
+      });
+    }
+
+    if (!email) {
+      return res.send({
+        success: false,
+        message: 'Error: Email cannot be blank.'
+      });
+    }
+    if (!password) {
+      return res.send({
+        success: false,
+        message: 'Error: Password cannot be blank.'
+      });
+    }
+    email = email.toLowerCase();
+    email = email.trim();
+    // Steps:
+    // 1. Verify email doesn't exist
+    // 2. Save
+    User.find({
+      email: email
+    }, (err, previousUsers) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error'
+        });
+      } else if (previousUsers.length > 0) {
+        return res.send({
+          success: false,
+          message: 'Error: Account already exist.'
+        });
+      }
+      // Save the new user
+      const newUser = new User();
+      newUser.email = email;
+      newUser.lastName = lastName;
+      newUser.firstName = firstName;
+      newUser.password = newUser.generateHash(password);
+      newUser.save((err, user) => {
+        if (err) {
+          return res.send({
+            success: false,
+            message: 'Error: Server error'
+          });
+        }
+        return res.send({
+          success: true,
+          message: 'Signed up'
+        });
+      });
+    });
+  }); // end of sign up endpoint
+};
