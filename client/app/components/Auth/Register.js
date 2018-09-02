f27bbdce419f6f11ca8b5f85c3b629de4192d41f
hexie <hx3329@gmail.com>
2018-09-02 21:25:14 +1000

    build register api

--- client/app/components/Auth/Register.js
+++ client/app/components/Auth/Register.js
@@ -15,23 +15,28 @@ const Style = {
 class SignUpPage extends React.Component{
   constructor(props) {
     super(props);
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.state = {
-      firstName:'',
-      lastName:'',
-      email:'',
-      password:'',
-      SignUpError:'',
+      signUpFirstName:'',
+      signUpLastName:'',
+      signUpEmail:'',
+      signUpPassword:'',
+      signUpError:'',
       isLoading:true,
       token:'',
     };
+
+    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+    this.handleLastNameChange = this.handleLastNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+    this.onSignUp = this.onSignUp.bind(this);
   }
 
   componentDidMount(){
-    const token = getFromStorage('the_main_app');
-    if(token){
+    const object = getFromStorage('the_main_app');
+    if(object && object.token){
+      const {token} = object;
       //verify token
       fetch('/api/account/verify?token='+token)
         .then(res => res.json())
@@ -54,26 +59,80 @@ class SignUpPage extends React.Component{
     }
   }
 
-  //onchage of Name
-  handleNameChange(e){
-    this.setState({name:e.target.value})
+  //onchage of firstName
+  handleFirstNameChange(e){
+    this.setState({signUpFirstName:e.target.value})
+  }
+  //onchage of lastName
+  handleLastNameChange(e){
+    this.setState({signUpLastName:e.target.value})
   }
   //onChange of email
   handleEmailChange(e){
-    this.setState({email:e.target.value})
+    this.setState({signUpEmail:e.target.value})
   }
   //onchange of Password
   handlePasswordChange(e){
-    this.setState({password:e.target.value})
+    this.setState({signUpPassword:e.target.value})
   }
+
+  onSignUp(){
+    //grap state
+    const {
+      signUpFirstName,
+      signUpLastName,
+      signUpEmail,
+      signUpPassword
+    } = this.state;
+
+    this.setState({
+      isLoading:true
+    });
+
+    //Post request to backend
+    fetch('/api/account/signup',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+        body: JSON.stringify({
+          firstName: signUpFirstName,
+          lastName: signUpLastName,
+          email: signUpEmail,
+          password: signUpPassword
+      }),
+
+    }).then(res => res.json())
+      .then(json => {
+        if(json.success){
+          this.setState({
+            signUpError: json.message,
+            isLoading:false,
+            //input box empty
+            signUpEmail:'',
+            signUpPassword:'',
+            signUpFirstName:'',
+            signUpLastName:''
+          });
+        }else{
+          this.setState({
+            signUpError: json.message,
+            isLoading: false
+          });
+        }
+      });
+  }
+
 
   render(){
     const {
       isLoading,
       token,
       signUpError,
-      email,
-      password
+      signUpFirstName,
+      signUpLastName,
+      signUpEmail,
+      signUpPassword
     } = this.state;
 
     if (isLoading){
@@ -83,6 +142,11 @@ class SignUpPage extends React.Component{
     if (!token) {
       return(
         <div style={Style}>
+          {
+            (signUpError)?(
+              <p>{signUpError}</p>
+            ):(null)
+          }
           <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
             <Grid.Column  style={{ maxWidth: 450 }}>
               <Image src='./logo2.jpg' size='small' verticalAlign='middle'/>
@@ -94,6 +158,7 @@ class SignUpPage extends React.Component{
                                 label='Email address'
                                 placeholder='Email address'
                                 type='email'
+                                value={signUpEmail}
                                 onChange={this.handleEmailChange}
                     />
                     <Form.Input
@@ -102,19 +167,31 @@ class SignUpPage extends React.Component{
                       label='Password'
                       placeholder='Password'
                       type='password'
+                      value={signUpPassword}
                       onChange={this.handlePasswordChange}
                     />
                   </Form.Field>
                   <Form.Group widths='equal'>
-                    <Form.Input type='name' label='First name' placeholder='First name'onChange={this.handleNameChange}/>
-                    <Form.Input label='Last name' placeholder='Last name'  />
+                    <Form.Input
+                      type='name'
+                      label='First name'
+                      placeholder='First name'
+                      value={signUpFirstName}
+                      onChange={this.handleFirstNameChange}
+                    />
+                    <Form.Input
+                      type='name'
+                      label='Last name'
+                      placeholder='Last name'
+                      value={signUpLastName}
+                      onChange={this.handleLastNameChange} />
                   </Form.Group>
                   <Form.Field>
                     <Form.Input label='Address' placeholder='Address' />
                     <Form.Input icon='mobile alternate' iconPosition='left' label='Phone' placeholder='Phone' />
                   </Form.Field>
                   <Form.Checkbox label='I agree to the Terms and Conditions' />
-                  <Button primary>SignUp</Button>
+                  <Button onClick={this.onSignUp} primary>SignUp</Button>
                 </Segment>
               </Form>
             </Grid.Column>
